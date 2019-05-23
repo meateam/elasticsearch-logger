53290d470d885c79cc3614d45eeb422aac5e35bc
Omri Shtamberger <omrishtam@gmail.com>
2019-05-23 11:43:52 +0300

    add environment variable configuration documentation to doc.go
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- doc.go
+++ doc.go
@@ -1,6 +1,19 @@
 // Package logger is used to log all intercepted stream calls.
 // The package exports the `NewLogger` function which sets up a logger with
 // the elogrus hook and returns it.
+// The logger can be configured using environment variables:
+//
+// LOG_LEVEL (*default: "error"*) - Defines the log level of the logger, possible values:
+// 	"panic", "fatal", "error", "warn", "warning", "info", "debug", "trace"
+//
+// LOG_INDEX (*default: "log"*) - Defines the index to which the logs would be indexed to,
+// 	the logs would be indexed to index pattern LOG_INDEX-*,
+// 	where * is the current day's date in the format 'YYYY.MM.DD'
+//
+// ELASTICSEARCH_URL (*default: http://localhost:9200*) - Defines the url of the elasticsearch server to index the logs to.
+//
+// HOST_NAME (*default: executable name*) - Defines the host name of the server that is using the logger, which will
+// 	be logged under 'Host' field.
 //
 // There's the `WithElasticsearchServerLogger` function which sets up a `grpc.ServerOption` to intercept streams with
 // `*logrus.Entry` of the logger, created with `NewLogger`, and the options given to it.
